629ebe2bbb25cfb6f2802659dd4894530fb44e78
Frank Flores <frankthetank67@gmail.com>
2020-03-07 23:53:31 -0800

    added gameStart function. updated the numberToGuess div and total score div with counter resetting back to zero after every win or lose. still having trouble setting each of the crystals values to a random number from the numberOptions array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,54 @@
-$( document ).ready(function() {
-   
-var targetNumber = Math.floor((Math.random() * 120) + 19);
-console.log(targetNumber);
+$(document).ready(function() {
+  var counter = 0;
+  var wins = 0;
+  var losses = 0;
+  var numberOptions = [];
 
-var counter = 0;
-var wins = 0;
-var losses = 0;
-var numberOptions = [];
+  function gameStart() {
+    counter = 0;
+    targetNumber = Math.floor(Math.random() * 120 + 19);
+    console.log(targetNumber);
+    $("#number-to-guess").text(targetNumber);
+    $("#total-score-counter").text(counter);
+  }
+  gameStart();
 
-$("#number-to-guess").text(targetNumber);
-$("#total-score-counter").text(counter);
-$("#wins-counter").text(wins);
-$("#losses-counter").text(losses);
+  $("#number-to-guess").text(targetNumber);
+  $("#total-score-counter").text(counter);
+  $("#wins-counter").text(wins);
+  $("#losses-counter").text(losses);
 
-for (var i = 0; i < 4; i++) {
-	var randomNumGuess = Math.floor((Math.random()*12) + 1);
-	numberOptions.push(randomNumGuess);
-	console.log(numberOptions[i]);
+  for (var i = 0; i < 4; i++) {
+    var randomNumGuess = Math.floor(Math.random() * 12 + 1);
+    numberOptions.push(randomNumGuess);
+    console.log(numberOptions[i]);
 
-	$(".crystal-image").attr("data-crystalvalue", numberOptions[i]);
-}
+    // This is setting the attribute and last iteration of the numberOptions array to all 4 images, still trying to figure out how to set each image to the different numbers off the array
+    $(".crystal-image").attr("data-crystalvalue", numberOptions[i]);
+  }
 
-$(".crystal-image").on("click", function() {
+  // tried using .each method to loop through each image and assign it a random value from the numberOptions array, but produces NaN and I'm not sure why.
+  /*$.each($(".crystal-image"),function() {
+	$(this).attr("data-crystalvalue", numberOptions[i]);
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+})*/
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
+  $(".crystal-image").on("click", function() {
+    var crystalValue = $(this).attr("data-crystalvalue");
     crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
+
     counter += crystalValue;
 
-	// 
-	$("#total-score-counter").text(counter);
+    $("#total-score-counter").text(counter);
 
     if (counter === targetNumber) {
-		wins++;
-		$("#wins-counter").text(wins);
-      }
-
-    else if (counter >= targetNumber) {
-		losses++;
-		$("#losses-counter").text(losses);
-	}
+      wins++;
+      $("#wins-counter").text(wins);
+      gameStart();
+    } else if (counter >= targetNumber) {
+      losses++;
+      $("#losses-counter").text(losses);
+      gameStart();
+    }
+  });
 });
-/*function gameStart() {
-	$(".random-number").html(randomNum);
-
-	$("#red").click(function() {
-		//$(this).
-	})
-}
-
-gameStart();*/
-});
-
